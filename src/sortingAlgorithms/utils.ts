7baa069470d1aa4575269d58bd37c7f967581c84
ZackMurray89 <zackmurray.dev@gmail.com>
2025-07-25 00:03:37 -0500

    05: Switch To Mac

---
+++ src/sortingAlgorithms/utils.ts
@@ -0,0 +1,26 @@
+//* Helpers & Utilities
+
+/**
+ *
+ * //* Generates A Random
+ *
+ *    Generates a random number within a specified range to be used with
+ *    algorithms
+ *
+ * @param size - Desired size of array
+ * @param min - Minimum possible value for random number
+ * @param max - Maximum possible value for random number
+ *
+ * @returns - A new array of unique random numbers | Sorted for binary search
+ *
+ */
+
+export function generateRandomNumber() {}
+
+/**
+ *
+ * //* Measure Execution Time
+ *
+ *    Measure the execution time of a given algorithm to find its
+ *    time and space complexity
+ */
